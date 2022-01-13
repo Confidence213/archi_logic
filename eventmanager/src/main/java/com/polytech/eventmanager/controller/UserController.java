47b02cf669f11935def192a1ebb9e5273b826eff
jackcolin21307 <jackcolin21307@gmail.com>
2022-01-13 11:53:14 +0100

    crud for user

--- eventmanager/src/main/java/com/polytech/eventmanager/controller/UserController.java
+++ eventmanager/src/main/java/com/polytech/eventmanager/controller/UserController.java
@@ -2,10 +2,9 @@ package com.polytech.eventmanager.controller;
 
 import com.polytech.eventmanager.model.User;
 import com.polytech.eventmanager.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +23,25 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // todo: add other methods
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable long userId) {
+        User user = userService.getUserById(userId);
+        if (user == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        if (createdUser == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<User> deleteUser(@PathVariable long userId) {
+        boolean status = userService.deleteUserById(userId);
+        if (!status) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.ok().build();
+    }
 
 }
--- eventmanager/src/main/java/com/polytech/eventmanager/service/UserService.java
+++ eventmanager/src/main/java/com/polytech/eventmanager/service/UserService.java
@@ -1,7 +1,6 @@
 package com.polytech.eventmanager.service;
 
 import com.polytech.eventmanager.model.User;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,20 +9,54 @@ import java.util.List;
 @Service
 public class UserService {
 
-    public List<User> getAllUsers() {
+    public List<User> list = new ArrayList<>();
+    public Integer id = 1;
+
+    public UserService() {
         User user1 = new User();
         user1.setNickname("user1");
         user1.setEmailAddress("mail@test.com");
+        user1.setId(this.id++);
 
         User user2 = new User();
         user2.setNickname("user2");
         user2.setEmailAddress("mail2@test.com");
+        user2.setId(this.id++);
 
-        List<User> list = new ArrayList<>();
-        list.add(user1);
-        list.add(user2);
-
-        return list;
+        this.list.add(user1);
+        this.list.add(user2);
     }
+
+    public List<User> getAllUsers() {
+        return this.list;
+    }
+
+    public User getUserById(long id) {
+        for (User user : this.list) {
+            if (user.getId() == id) return user;
+        }
+        return null;
+    }
+
+    public User createUser(User givenUser) {
+        if (givenUser.getNickname() != null && givenUser.getEmailAddress() != null) {
+            givenUser.setId(this.id++);
+            this.list.add(givenUser);
+            return givenUser;
+        }
+        return null;
+    }
+
+    public boolean deleteUserById(long id) {
+        for (User user : this.list) {
+            if (user.getId() == id) {
+                this.list.remove(user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
